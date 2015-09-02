47254eba441cf2e834fd11c6a8a2fb22f84f09bd
FaithInMotion <clk.faithinmotion@gmail.com>
2015-09-02 15:46:48 -0400

    Created migration and model for Scenery

---
+++ lumen/database/migrations/2015_09_02_192436_create_scenery_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSceneryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('story_scenery');
+        Schema::create('story_scenery', function($table)
+        {
+            $table->increments('id');
+            $table->integer('story_id')->unsigned();
+            $table->string('name', 200);
+            $table->string('description', 255);
+            $table->timestamps();
+        });
+
+        Schema::table('story_scenery', function($table)
+        {
+            $table->foreign('story_id')->references('id')->on('user_stories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('story_scenery');
+    }
+}
