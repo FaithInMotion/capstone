3809da13dabb1ec89612e32467330f64c3faf559
sbutbul <sbutbul@g.coastal.edu>
2015-10-20 14:06:22 -0400

    form password fields fixed

--- project/resources/views/auth/register.blade.php
+++ project/resources/views/auth/register.blade.php
@@ -10,22 +10,30 @@
 
                 {!! csrf_field() !!}
 
-                <h1 class="basic-grey">Register</h1>
+                <h1 class="basic-grey">Register
+                    <span>Please fill in all the fields.</span>
+                </h1>
 
-                <label for="name">Name:</label>
+                <label for="name"><span>Name:</span>
                 <input type="text" name="name" id="name" value="{{ old('name') }}" required>
+                </label>
 
-                <label for="email">Email:</label>
+                <label for="email"><span>Email:</span>
                 <input type="email" name="email" id="email" value="{{ old('email') }}" required>
+                </label>
 
-                <label for="password">Password:</label>
+                <label for="password"><span>Password:</span>
                 <input type="password" name="password" id="password" required>
+                </label>
 
-                <label for="password_confirmation">Confirm Password:</label>
+                <label for="password_confirmation"><span>Confirm Password:</span>
                 <input type="password" name="password_confirmation" id="password_confirmation" required>
+                </label>
 
-                <br>
+                <label>
+                    <span></span>
                 <button type="submit" class="button">Register</button>
+                </label>
 
             </form>
         </div>
