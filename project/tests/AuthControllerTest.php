888813e3554d81c0a6322fb1ed9419a9964dcdf4
jbrice21 <ajbrice@coastal.edu>
2015-11-11 20:54:02 -0800

    new test

---
+++ project/tests/AuthControllerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class AuthControllerTest extends TestCase
+{
+    /** @test */
+    public function user_registration()
+    {
+        $this->visit('auth/register.blade');
+        $this->type('JaQuise', 'name');
+        $this->type('ajbrice@coastal.edu', 'email');
+        $this->type('antonio12', 'password');
+        $this->type('antonio12', 'confirmation_password');
+        $this->press('submit');
+    }
+
+    public function user_login()
+    {
+        $this->visit('auth/login.blade');
+        $this->type('ajbrice@coastal.edu', 'email');
+        $this->type('antonio12', 'password');
+        $this->check('remember');
+        $this->press('submit');
+    }
+
+}
